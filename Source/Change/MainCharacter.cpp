054fcf16a4e30b132cb0d6603245433571f54566
25CU0328 <25cu0328@jec.ac.jp>
2026-09-23 15:33:48 +0900

    [Add] プレイヤーの追加
    プレイヤーを表すMainCharacterクラスを作成し、簡単な移動処理を実装
    MainCharacterクラスに対応するBlueprintを作成し、デフォルトポーンとして設定

---
+++ Source/Change/MainCharacter.cpp
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacter.h"
+#include "Kismet/KismetSystemLibrary.h" // 追加
+#include "Kismet/GameplayStatics.h" // 追加
+#include "Components/InputComponent.h" // 追加
+#include "EnhancedInputComponent.h" // 追加
+#include "EnhancedInputSubsystems.h" // 追加
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+    // プレイヤーコントローラーにキャストできないなら処理しない
+    APlayerController* playerController = Cast<APlayerController>(Controller);
+    if (!playerController)
+        return;
+
+    // 入力を有効にする
+    EnableInput(playerController);
+
+    // Subsystemを取得できないなら処理しない
+    UEnhancedInputLocalPlayerSubsystem* subsystem =
+        ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer());
+    if (!subsystem)
+        return;
+
+   
+
+    // IMCが設定されたなら
+    if (m_mappingContext) {
+        // SubsystemにIMCを追加する
+        subsystem->AddMappingContext(m_mappingContext, 0);
+    }
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* _pPlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(_pPlayerInputComponent);
+
+    if (UEnhancedInputComponent* pEnhancedInputComponent = CastChecked<UEnhancedInputComponent>(_pPlayerInputComponent)) {
+
+        // 移動処理と入力のバインド
+        pEnhancedInputComponent->BindAction(
+            m_IA_playerMove, 
+            ETriggerEvent::Triggered,
+            this,
+            &AMainCharacter::UpdateMovement
+        );
+    }
+}
+
+// プレイヤーの移動処理
+void AMainCharacter::UpdateMovement(const FInputActionValue& _value)
+{
+	// キーボードの入力を取得する
+	FVector MovementVector = _value.Get<FVector>();
+
+    if (Controller != nullptr)
+    {
+        // X 軸移動
+        AddMovementInput(FVector(1.0f, 0.0f, 0.0f), MovementVector.X);
+
+        // Z 軸移動
+        AddMovementInput(FVector(0.0f, 0.0f, 1.0f), MovementVector.Z);
+    }
+}
+
+
+// 変形ボタンが押されたばかり
+void AMainCharacter::OnTransformStarted(const FInputActionValue& _value)
+{
+    UE_LOG(LogTemp, Log, TEXT("Transform Start"));
+    // 変身開始アニメションやエフェクト
+}
+// 一定時間押されたら
+void AMainCharacter::OnTransformCompleted(const FInputActionValue& _value)
+{
+    UE_LOG(LogTemp, Log, TEXT("Transform Complete"));
+    // 変身アニメション
+}
+// 途中で離されたら
+void AMainCharacter::OnTransformCanceled(const FInputActionValue& _value)
+{
+    UE_LOG(LogTemp, Log, TEXT("Transform Canceled"));
+    // 変身諦めアニメションやエフェクト
+}
---
+++ Source/Change/MainCharacter.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputMappingContext.h" // 追加
+#include "InputAction.h" // 追加
+#include "InputActionValue.h" // 追加
+#include "MainCharacter.generated.h"
+
+
+
+// 入力関連クラスの前方宣言
+class UIInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class CHANGE_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* _playerInputComponent) override;
+
+	// 入力関連
+private:
+	// IAのマッピング
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* m_mappingContext;
+
+	// 移動のIA
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* m_IA_playerMove;
+
+	// プレイヤーの移動処理
+	void UpdateMovement(const FInputActionValue& _value);
+
+
+	// 変形のIA
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* m_IA_Transform;
+	// 変形ボタンが押されたばかり
+	void OnTransformStarted(const FInputActionValue& _value);
+	// 一定時間押されたら
+	void OnTransformCompleted(const FInputActionValue& _value); 
+	// 途中で離されたら
+	void OnTransformCanceled(const FInputActionValue& _value); 
+};
